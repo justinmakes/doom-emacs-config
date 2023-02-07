cd9bde63a8fae7faab813ec29f18a097cd94239c
justinr <justin@worldcrafters.io>
2023-02-07 02:27:12 -0700

    Org roam and mu4e functional.

--- config.el
+++ config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "Justin R"
+      user-mail-address "justin@worldcrafters.io")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
@@ -74,3 +74,140 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+(setq
+ projectile-project-search-path '("~/projects/" "~/projects/rust/"))
+
+;; Org Config
+(setq org-roam-directory "~/org-roam")
+;; TODO: Figure out how to bind org roam sync to a keybinding!
+
+;; Default Mode Settings:
+;; (global-page-break-lines-mode -1) ;; disable to fix issue with node backlinks buffer. Can also org-roam-buffer-refresh
+
+;; dailies capture will record time of capture
+;; check format-time-string docs for more info
+(setq org-roam-dailies-capture-templates
+      '(("d" "default" entry "* %<%H:%M>: %?"
+         :if-new (file+head "%<%Y-%m-%d>.org" "#+title: %<%Y-%m-%d>\n"))))
+
+(setq org-agenda-files '("~/org-roam/" "~/org-roam/mail/" "~/org-roam/daily/"))
+
+     ;; set default printer
+(setq printer-name "HP_Color_LaserJet_Pro_m453-4")
+
+;; mu4e-settings
+(require 'mu4e)
+;; (require 'smtpmail)
+(setq mu4e-root-maildir "~/.mail"
+      mu4e-get-mail-command "mbsync --config /home/justinr/.config/mbsync/config -a"
+      mu4e-attachment-dir "~/attachments"
+      message-send-mail-function 'smtpmail-send-it
+      mu4e-compose-format-flowed t
+      mml-secure-openpgp-signers '("4B78B6FF1ECB8DE3FB6703850399E1EEC9095742"))
+(add-hook 'message-send-hook 'mml-secure-message-sign-pgpmime) ;; sign all outgoing mail with your public key fingerprint
+;; NOTE: format-flowed makes word wrapping on plain text emails work properly across different types of devices
+(setq mu4e-bookmarks
+      `((:name "All inboxes" :query "maildir:/apsis/INBOX OR maildir:/remoterealty/INBOX OR maildir:/worldcrafters/INBOX" :key ?i)
+        (:name "All sent" :query "maildir:/apsis/Sent OR maildir:/remoterealty/Sent OR maildir:/worldcrafters/Sent" :key ?s)
+        (:name "All drafts" :query "maildir:/apsis/Drafts OR maildir:/remoterealty/Drafts OR maildir:/worldcrafters/Drafts" :key ?d)
+        (:name "All repos" :query "maildir:/apsis/Repo OR maildir:/remoterealty/Repo OR maildir:/worldcrafters/Repo" :key ?r)
+        (:name "All trash" :query "maildir:/apsis/Trash OR maildir:/remoterealty/Trash OR maildir:/worldcrafters/Trash" :key ?g)
+        (:name "All spam" :query "maildir:/apsis/Spam OR maildir:/remoterealty/Spam OR maildir:/worldcrafters/Spam" :key ?j)
+        ;; ("flag:unread AND NOT flag:trashed" "Unread messages" ?u)
+        ("flag:unread AND NOT maildir:/apsis/Trash AND NOT maildir:/remoterealty/Trash AND NOT maildir:/worldcrafters/Trash" "Unread messages" ?u)
+        ("date:today..now" "Today's messages" ?t)
+        ("date:7d..now" "Last 7 days" ?w)
+        ("mime:image/*" "Messages with images" ?p)
+        (,(mapconcat 'identity
+                     (mapcar
+                      (lambda (maildir)
+                        (concat "maildir:" (car maildir)))
+                      mu4e-maildir-shortcuts) " OR ")
+         "All mail" ?a)))
+
+(setq mu4e-contexts
+      `(,(make-mu4e-context
+           :name "worldcrafters"
+           :match-func
+           (lambda (msg)
+             (when msg
+               (string-prefix-p "/worldcrafters" (mu4e-message-field msg :maildir))))
+           :vars '((user-mail-address . "justin@worldcrafters.io")
+                   (user-full-name . "Justin Ramos")
+                   (smtpmail-smtp-user . "justin@worldcrafters.io")
+                   (smtpmail-smtp-server . "mail.privateemail.com")
+                   (smtpmail-smtp-service . 465)
+                   (smtpmail-stream-type . ssl)
+                   (mu4e-compose-signature . "Justin")
+                   (mu4e-drafts-folder . "/worldcrafters/Drafts")
+                   (mu4e-refile-folder . "/worldcrafters/Repo")
+                   (mu4e-sent-folder . "/worldcrafters/Sent")
+                   (mu4e-trash-folder . "/worldcrafters/Trash")))
+        ,(make-mu4e-context
+          :name "apsis"
+          :match-func
+          (lambda (msg)
+            (when msg
+              (string-prefix-p "/apsis" (mu4e-message-field msg :maildir))))
+          :vars '((user-mail-address . "justin@apsis.co")
+                  (user-full-name . "Justin Ramos")
+                  (smtpmail-smtp-user . "justin@apsis.co")
+                  (smtpmail-smtp-server . "mail.privateemail.com")
+                  (smtpmail-smtp-service . 465)
+                  (smtpmail-stream-type . ssl)
+                  (mu4e-compose-signature . "Justin")
+                  (mu4e-drafts-folder . "/apsis/Drafts")
+                  (mu4e-refile-folder . "/apsis/Repo")
+                  (mu4e-sent-folder . "/apsis/Sent")
+                  (mu4e-trash-folder . "/apsis/Trash")))
+        ,(make-mu4e-context
+          :name "remoteRealty"
+          :match-func
+          (lambda (msg)
+            (when msg
+              (string-prefix-p "/remoterealty" (mu4e-message-field msg :maildir))))
+          :vars '((user-mail-address . "justin@remoterealty.com")
+                  (user-full-name . "Justin Ramos")
+                  (smtpmail-smtp-user . "justin@remoterealty.com")
+                  (smtpmail-smtp-server . "mail.privateemail.com")
+                  (smtpmail-smtp-service . 465)
+                  (smtpmail-stream-type . ssl)
+                  (mu4e-compose-signature . "Justin R\nOperator - Remote Realty LLC\n480-333-0533\njustin@remoterealty.com\nremoterealty.com")
+                  (mu4e-drafts-folder . "/remoterealty/Drafts")
+                  (mu4e-refile-folder . "/remoterealty/Repo")
+                  (mu4e-sent-folder . "/remoterealty/Sent")
+                  (mu4e-trash-folder . "/remoterealty/Trash")))))
+
+;; org-mode mail capture templates
+(setq org-capture-templates
+      '(("m" "Email Workflow")
+        ("mf" "Follow up" entry (file+olp "~/org-roam/mail/mail.org" "Follow Up")
+         "* TODO Follow up with %:fromname: %a\nSCHEDULED:%t\nDEADLINE:%(org-insert-time-stamp (org-read-date nil t \"+2d\"))\n%i" :immediate-finish t)
+        ("mr" "Read Later" entry (file+olp "~/org-roam/mail/mail.org" "Read Later")
+         "* TODO Read later: %a\nSCHEDULED:%t\n%i" :immediate-finish t)))
+
+;; add custom mu4e actions for our capture templates
+(defun efs/capture-mail-follow-up (msg)
+  (interactive)
+  (call-interactively 'org-store-link)
+  (org-capture nil "mf"))
+
+(defun efs/capture-mail-read-later (msg)
+  (interactive)
+  (call-interactively 'org-store-link)
+  (org-capture nil "mr"))
+
+;; Add custom actions for our capture templates
+(add-to-list 'mu4e-headers-actions
+             '("follow up" . efs/capture-mail-follow-up) t)
+(add-to-list 'mu4e-view-actions
+             '("follow up" . efs/capture-mail-follow-up) t)
+(add-to-list 'mu4e-headers-actions
+             '("read later" . efs/capture-mail-read-later) t)
+(add-to-list 'mu4e-view-actions
+             '("read later" . efs/capture-mail-read-later) t)
+
+;; pinentry setup
+;; (require 'epg)
+;; (setq epg-pinentry-mode 'loopback)
--- init.el
+++ init.el
@@ -35,17 +35,17 @@
        ;;(emoji +unicode)  ; 🙂
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
-       ;;indent-guides     ; highlighted indent columns
-       ;;ligatures         ; ligatures and symbols to make your code pretty again
+       indent-guides     ; highlighted indent columns
+       ligatures         ; ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
        ;;nav-flash         ; blink cursor line after big motions
        ;;neotree           ; a project drawer, like NERDTree for vim
        ophints           ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
-       ;;tabs              ; a tab bar for Emacs
-       ;;treemacs          ; a project drawer, like neotree but cooler
-       ;;unicode           ; extended unicode support for various languages
+       tabs              ; a tab bar for Emacs
+       treemacs          ; a project drawer, like neotree but cooler
+       unicode           ; extended unicode support for various languages
        (vc-gutter +pretty) ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        ;;window-select     ; visually switch windows
@@ -61,7 +61,7 @@
        ;;lispy             ; vim for lisp, for people who don't like vim
        ;;multiple-cursors  ; editing in many places at once
        ;;objed             ; text object editing for the innocent
-       ;;parinfer          ; turn lisp into python, sort of
+       parinfer          ; turn lisp into python, sort of
        ;;rotate-text       ; cycle region at point between text candidates
        snippets          ; my elves. They type so I don't have to
        ;;word-wrap         ; soft wrapping with language-aware indent
@@ -74,10 +74,10 @@
        vc                ; version-control and Emacs, sitting in a tree
 
        :term
-       ;;eshell            ; the elisp shell that works everywhere
+       eshell            ; the elisp shell that works everywhere
        ;;shell             ; simple shell REPL for Emacs
        ;;term              ; basic terminal emulator for Emacs
-       ;;vterm             ; the best terminal emulation in Emacs
+       ;; vterm             ; the best terminal emulation in Emacs
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
@@ -95,7 +95,7 @@
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
-       ;;lsp               ; M-x vscode
+       lsp               ; M-x vscode
        magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
@@ -105,7 +105,7 @@
        ;;taskrunner        ; taskrunner for all your projects
        ;;terraform         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
-       ;;tree-sitter       ; syntax and parsing, sitting in a tree...
+       tree-sitter       ; syntax and parsing, sitting in a tree...
        ;;upload            ; map local to remote projects via ssh/ftp
 
        :os
@@ -115,9 +115,9 @@
        :lang
        ;;agda              ; types of types of types of types...
        ;;beancount         ; mind the GAAP
-       ;;(cc +lsp)         ; C > C++ == 1
+       (cc +lsp)         ; C > C++ == 1
        ;;clojure           ; java with a lisp
-       ;;common-lisp       ; if you've seen one lisp, you've seen them all
+       common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
        ;;csharp            ; unity, .NET, and mono shenanigans
@@ -134,7 +134,7 @@
        ;;fortran           ; in FORTRAN, GOD is REAL (unless declared INTEGER)
        ;;fsharp            ; ML stands for Microsoft's Language
        ;;fstar             ; (dependent) types and (monadic) effects and Z3
-       ;;gdscript          ; the language you waited for
+       gdscript          ; the language you waited for
        ;;(go +lsp)         ; the hipster dialect
        ;;(graphql +lsp)    ; Give queries a REST
        ;;(haskell +lsp)    ; a language that's lazier than I am
@@ -148,23 +148,25 @@
        ;;latex             ; writing papers in Emacs has never been so fun
        ;;lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
-       ;;lua               ; one-based indices? one-based indices
+       lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
-       ;;nix               ; I hereby declare "nix geht mehr!"
+       nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       org               ; organize your plain life in plain text
+       (org
+        +pretty
+        +roam2)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       ;;python            ; beautiful is better than ugly
+       python            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
        ;;rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       ;;(rust +lsp)       ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       (rust +lsp)       ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;(scheme +guile)   ; a fully conniving family of lisps
        sh                ; she sells {ba,z,fi}sh shells on the C xor
@@ -177,7 +179,7 @@
        ;;zig               ; C, but simpler
 
        :email
-       ;;(mu4e +org +gmail)
+       (mu4e +org +gmail)
        ;;notmuch
        ;;(wanderlust +gmail)
 
