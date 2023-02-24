696d5511666c8400439b225a1442092895f3b777
justinr <justin@worldcrafters.io>
2023-02-23 23:14:23 -0700

    Added state tracking to 'MAYB' keyword

--- config.el
+++ config.el
@@ -97,7 +97,7 @@
 ;; (after! org)
 (setq org-todo-keywords
       ;; NOTE: Keywords after "|" will have a "done" state
-      '((sequence "TRAY(i)" "WAIT(w@/!)" "NACT(n)" "PROJ(p@/!)" "SPRJ(P@/!)" "SCAN(l!)" "MAYB(f)" "|" "REFR(r!)" "DONE(d!)" "CANC(c@)" "NO(n)")
+      '((sequence "TRAY(i)" "WAIT(w@/!)" "NACT(n)" "PROJ(p@/!)" "SPRJ(P@/!)" "SCAN(l!)" "MAYB(f!)" "|" "REFR(r!)" "DONE(d!)" "CANC(c@)" "NO(n)")
         (sequence "[ ](T)" "|" "[X](D!)")))
         ;; (sequence "[ ](T)" "[-](A)" "[?](M)" "|" "[X](D)")))
 (setq org-todo-keyword-faces
